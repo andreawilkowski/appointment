f291fc7114d94834033055e4a23174f79b075a98
Andrea Wilkowski and Isabel Hasse <andrea@>
2017-10-16 20:18:22 -0700

    add html for questions about name, favorite food, and favorite music.

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,8 @@
 $(document).ready(function(){
   $("form").submit(function(event){
     debugger;
-    var nameInput = $("input#name").val();
-    var dateInput = $("input#date").val();
-    var starttimeInput = $("input#start-time").val();
-    var endtimeInput = $("input#end-time").val();
-
-    $(".name").text(nameInput);
-    $(".date").text(dateInput);
-    $(".start-time").text(starttimeInput);
-    $(".end-time").text(endtimeInput);
-
-    $("#confirmation").show();
-
+    var food = $("#food").val();
+    var flavor = $("input:radio[name=music]:checked").val();
     event.preventDefault();
   });
 });
