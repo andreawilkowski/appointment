ef28379b0d980e64bf8c275ded4a60c61dec35fe
Andrea Wilkowski and Isabel Hasse <isabel@>
2017-10-16 20:35:20 -0700

    add radio, color, date, and js.

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,18 @@
 $(document).ready(function(){
   $("form").submit(function(event){
     debugger;
+    var name = $("#name").val();
     var food = $("#food").val();
     var flavor = $("input:radio[name=music]:checked").val();
+    var dob = $("#born").val();
+    var favoriteColor = $("#color").val();
+
+    $("p").append(name);
+    $("p").append(food);
+    $("p").append(flavor);
+    $("p").append(dob);
+    $("p").append(favoriteColor);
     event.preventDefault();
+
   });
 });
